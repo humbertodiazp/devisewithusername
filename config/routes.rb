af77e212e121333d3f5f9b67851c0fdb49bb5acb
Humberto <hdkaratechop@gmail.com>
2023-02-11 23:16:06 -0800

    define home as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'main#home'
+  devise_for :users
+   root 'main#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
